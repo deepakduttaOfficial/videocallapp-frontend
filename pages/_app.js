894ffbb042b8e5c9f4a0cc379e92581b1bd65088
Deepak Dutta <coder.deepakyt@gmail.com>
2023-02-19 22:21:03 +0530

    Intrigate socket.io

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import { ContextProvider } from "@/socketio/SocketContext";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ContextProvider>
+      <Component {...pageProps} />
+    </ContextProvider>
+  );
 }
